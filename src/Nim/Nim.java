fcf117dc356b6f27a0e1133ab04b415d76f64323
NCG\per.haagensen <per.haagensen@novanet.no>
2024-02-06 16:00:30 +0100

    camel case on props

--- src/Nim/Nim.java
+++ src/Nim/Nim.java
@@ -5,26 +5,26 @@ import java.util.ArrayList;
 
 public class Nim {
 
-    private ArrayList<Pile> Piles = new ArrayList<>();
+    private final ArrayList<Pile> piles = new ArrayList<>();
 
     public Nim(int pileSize){
         for(int i = 0; i < 3; i++){
-            Piles.add(new Pile(pileSize));
+            piles.add(new Pile(pileSize));
         }
     }
 
     public void removePieces(int number, int targetPile){
-        var pile = Piles.get(targetPile);
+        var pile = piles.get(targetPile);
         pile.setAmount(number);
     }
 
     public int getRemainingPileAmount(int pileNo){
-        return Piles.get(pileNo).getAmount();
+        return piles.get(pileNo).getAmount();
     }
 
     @Override
     public String toString() {
         return MessageFormat.format("Nim'{'Pile 1: {0} | Pile 2: {1} | Pile 3: {2} | '}'",
-                Piles.get(0).getAmount(), Piles.get(1).getAmount(), Piles.get(2).getAmount());
+                piles.get(0).getAmount(), piles.get(1).getAmount(), piles.get(2).getAmount());
     }
 }
